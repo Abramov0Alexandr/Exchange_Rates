cc5b8cffc4046958fd0a1c950571f9f391f9768b
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-03-29 01:04:40 +0300

    Написал тестовый код для парсинга курса валют

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import requests
+import os
 
+# "apikey": "3TVA2WYarSx30gtHlNsUF5umTSABZ6Pz"
+
+
+url = "https://api.apilayer.com/exchangerates_data/convert?to=USD&from=EUR&amount=1"
+# VAL_KEY = os.getenv('VAL_KEY')
+VAL_KEY = '3TVA2WYarSx30gtHlNsUF5umTSABZ6Pz'
+payload = {}
+headers = {'apikey': VAL_KEY}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+
+status_code = response.status_code
+result = response.text
+print(status_code)
+print(result)
